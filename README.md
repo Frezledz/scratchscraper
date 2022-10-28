a3c995bae9e571eb61070b6e8bcd3d04bc056d69
xxxfreezerxxx <freezerfx512@gmail.com>
2022-10-28 11:45:53 +0100

    first

---
+++ README.md
@@ -0,0 +1,2 @@
+# scratchscraper
+# scratchscraper
---
+++ index.js
@@ -0,0 +1,11 @@
+const a = ()=>{
+    console.log("s");
+}
+
+const c = ()=>{
+    console.log("s");
+}
+module.exports = {
+    a:a,
+    b:c
+};
